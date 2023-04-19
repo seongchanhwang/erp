b7a3c75ea048caf18b15ba859ac9f85c76eed0c8
gytjq <gytjq456@naver.com>
2023-04-19 23:43:20 +0900

    login axios request

--- src/main/frontend/src/pages/Login.js
+++ src/main/frontend/src/pages/Login.js
@@ -1,44 +1,56 @@
 import React, { useEffect, useState } from "react";
 import styles from "../css/login.scss";
 import Background from "../components/login/Background";
+import axios from "axios";
 
 const Login = () => {
-  const [load, setLoad] = useState(false);
-  useEffect(() => {
-    setLoad(true);
-  }, []);
-  return (
-    <>
-      <div id="login-wrap" className="flex-box justify-cnt dir-col">
-        <div className={`bg ${load ? "active" : ""}`}>
-          <Background idx={1} />
-          <Background idx={2} />
-          <Background idx={3} />
-        </div>
-        <div className="txt-box">
-          <h2>Eventually</h2>
-          <p>
-            A simple template for telling the world when you'll launch <br />
-            your next big thing. Brought to you by HTML5 UP.
-          </p>
-        </div>
-        <form>
-          <div className="input-box">
-            <div>
-              <input type="text" placeholder="이메일을 입력해주세요"></input>
-            </div>
-            <div>
-              <input
-                type="password"
-                placeholder="비밀번호를 입력해주세요"
-              ></input>
-            </div>
-            <button className="btn-submit">Login</button>
-          </div>
-        </form>
-      </div>
-    </>
-  );
+	const [load, setLoad] = useState(false);
+	const [id, setId] = useState('');
+	const [pw, setPw] = useState('');
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(id, pw)
+		axios.post('/login')
+			.then((response) => console.log(response))
+		.catch((error) => console.log(error))
+	}
+	useEffect(() => {
+		setLoad(true);
+	}, []);
+	return (
+		<>
+			<div id="login-wrap" className="flex-box justify-cnt dir-col">
+				<div className={`bg ${load ? "active" : ""}`}>
+					<Background idx={1} />
+					<Background idx={2} />
+					<Background idx={3} />
+				</div>
+				<div className="txt-box">
+					<h2>Eventually</h2>
+					<p>
+						A simple template for telling the world when you'll launch <br />
+						your next big thing. Brought to you by HTML5 UP.
+					</p>
+				</div>
+				<form onSubmit={handleSubmit}>
+					<div className="input-box">
+						<div>
+							<input type="text" placeholder="이메일을 입력해주세요" value={id} onChange={e => setId(e.target.value)} ></input>
+						</div>
+						<div>
+							<input
+								type="password"
+								placeholder="비밀번호를 입력해주세요"
+								value={pw}
+								onChange={e => setPw(e.target.value)}
+							></input>
+						</div>
+						<button className="btn-submit">Login</button>
+					</div>
+				</form>
+			</div>
+		</>
+	);
 };
 
 export default Login;
