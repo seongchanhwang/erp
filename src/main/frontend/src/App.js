3ecf3915a0ea3a054c0592bb54b2e35992fe5b89
이효섭 <hslee2@kb-sys.co.kr>
2023-04-19 15:02:25 +0900

    first init

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,14 +2,29 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [message, setMessage] = useState("");
+  // const [message, setMessage] = useState("");
   useEffect(() => {
     axios
-      .get("/api/hello")
-      .then((response) => setMessage(response.data))
+      .get("/login", {
+        loginId: "admin",
+        password: "admin!2",
+      })
+      .then((response) => {
+        console.log(response);
+      })
       .catch((error) => console.log(error));
   }, []);
-  return <div>123 {message}</div>;
+  return (
+    <>
+      <div>123 </div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+      <div>456</div>
+    </>
+  );
 }
 
 export default App;
