81223d3519e6226995dfda537fe471b829f6d07e
gytjq <gytjq456@naver.com>
2023-04-21 02:04:57 +0900

    1. 로그인 페이지 백그라운드 애니메이션 수정 2. 로그인 정보 id,pw 객체 변환

--- src/main/frontend/src/components/login/Background.js
+++ src/main/frontend/src/components/login/Background.js
@@ -1,17 +1,25 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { CSSTransition, Transition } from "react-transition-group";
 
 const Background = ({ idx }) => {
   const [bgIdx, setBgIdx] = useState(1);
+  const [inProp, setInProp] = useState(false);
   useEffect(() => {
+    setInProp(idx === bgIdx ? true : false);
     const timer = setInterval(() => {
       setBgIdx(bgIdx >= 3 ? 1 : bgIdx + 1);
     }, 5000);
-    return () => clearInterval(timer);
+    return () => {
+      clearInterval(timer);
+    };
   }, [bgIdx]);
+
   return (
-    <article className={`item${idx} ${idx === bgIdx ? "active" : ""}`}>
-      {bgIdx}
-    </article>
+    <>
+      <CSSTransition in={inProp} timeout={8000} classNames="my-node">
+        <article className={`item${idx}`}>{bgIdx}</article>
+      </CSSTransition>
+    </>
   );
 };
 
